c86e3e26d063e136fbbaf269eddd422de105783b
Kashigu <78795585+Kashigu@users.noreply.github.com>
2024-05-05 22:06:54 +0100

    header

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,7 +5,6 @@ import Header from '@/components/main/Header';
 function Profiles() {
   return (
     <>
-      <Header />
       <div className="flex justify-center items-center h-full">
         <div className="w-full px-12 pt-12 z-0" style={{ width: `calc(100% - 300px)` }}>
           <div className='text-center'>
