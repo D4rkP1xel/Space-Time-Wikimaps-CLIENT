0432e5ca61a72be712915d2cc9740c6e43422367
Alexandre Silva <alexcruzsilva12@gmail.com>
2024-06-01 16:10:27 +0100

    refresh token working

--- utils/axios/axiosHandler.ts
+++ utils/axios/axiosHandler.ts
@@ -22,4 +22,43 @@ axiosHandler.interceptors.request.use(
     }
 )
 
+axiosHandler.interceptors.response.use(
+    response => {
+        return response;
+    },
+    async error => {
+        const originalRequest = error.config;
+
+        // Check if the error is due to an expired JWT
+        if (error.response && error.response.status === 401 && error.response.data === 'JWT expired') {
+            try {
+                // Get the refresh token from cookies
+                const refreshToken = Cookies.get('refreshToken');
+
+                // Make a request to refresh the token
+                const { data } = await axios.post(`${API_URL}/auth/refresh`, { token: refreshToken }, {
+                    headers: {
+                        'Authorization': `Bearer ${refreshToken}`
+                    }
+                });
+
+                // Update cookies with the new access token
+                Cookies.set('accessToken', data.token);
+
+                // Update the Authorization header with the new access token
+                originalRequest.headers['Authorization'] = `Bearer ${data.token}`;
+
+                // Retry the original request with the new access token
+                return axiosHandler(originalRequest);
+            } catch (refreshError) {
+                // If refreshing the token fails, reject with the refresh error
+                return Promise.reject(refreshError);
+            }
+        }
+
+        // If the error is not a 401 or not due to an expired JWT, reject with the original error
+        return Promise.reject(error);
+    }
+)
+
 export default axiosHandler
